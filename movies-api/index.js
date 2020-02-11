d8d157b1221d6483bc4b61194a2261e64bd81bbc
angelobarbara <angelo.iesgrancapitan@gmail.com>
2020-02-11 13:57:52 +0100

    services errorHandles validationHandler

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,9 +2,37 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const moviesApi = require('./routes/movies')
+const moviesApi = require('./routes/movies');
 
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+
+// body parser
+app.use(express.json())
+
+// routes
 moviesApi(app)
+
+// catch Error 404
+app.use(notFoundHandler)
+
+// erros middleware
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
+
+// create a rotating write stream
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'log')
+})
+
+//Logger
+app.use(morgan('combined', { stream: accessLogStream }))
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
